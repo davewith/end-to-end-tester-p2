3bfb29b09a21d6557319a0967c3d87254d562024
Dave <david.oneill@with-madrid.com>
2023-04-23 16:01:24 +0200

    First commit for logging in
    Make functionality for log in

---
+++ front/components/UserMessage.vue
@@ -0,0 +1,82 @@
+<template>
+    <div
+        v-if="message"
+        id="user-message"
+        :class="['user-message', message.type]"
+    >
+        {{ message.text }}
+    </div>
+</template>
+
+<script>
+/**
+ * @type {Object} UserMessage
+ * @property {string} text - The text of the message.
+ * @property {string} type - The type of the message. Can be "success" or "error".
+ */
+
+/**
+ * Vue component for displaying a user message.
+ * @component
+ *
+ * @prop {string} text - The text content of the message.
+ * @prop {string} type - The type of the message. Can be "success" or "error".
+ *
+ * @example
+ * // In a Vue template:
+ * <UserMessage ref="messageComponent"></UserMessage>
+ *
+ * // In a Vue method:
+ * this.$refs.messageComponent.showMessage("Log in successful!", "success");
+ */
+
+export default {
+    data() {
+        return {
+            /**
+             * The message to display. This is an object with two properties:
+             * - `text`: the text of the message
+             * - `type`: the type of message, either "success" or "error"
+             * @type {?UserMessage}
+             */
+            message: null,
+        };
+    },
+    methods: {
+        /**
+         * Display a message to the user.
+         * @param {string} text - The text of the message to display.
+         * @param {string} type - The type of the message to display. Can be "success", "warning", or "error".
+         */
+        showMessage(text, type) {
+            this.message = { text, type };
+            setTimeout(() => {
+                this.message = null;
+            }, 5000);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.user-message {
+    position: absolute;
+    top: 0;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    font-size: 1rem;
+    font-weight: bold;
+    margin: 1rem 0;
+    font-family: "roboto" sans-serif;
+}
+
+.user-message.error {
+    color: red;
+    background-color: #fdecea;
+}
+
+.user-message.success {
+    color: green;
+    background-color: #d4edda;
+}
+</style>
---
+++ front/pages/login.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="container">
+        <UserMessage ref="messageComponent" />
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input id="email" v-model="email" type="text" />
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input id="password" v-model="password" type="password" />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import UserMessage from "../components/UserMessage.vue";
+
+export default {
+    components: {
+        UserMessage,
+    },
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        async handleSubmit() {
+            if (this.email === "") {
+                this.$refs.messageComponent.showMessage(
+                    "Email cannot be empty.",
+                    "error"
+                );
+                return;
+            }
+
+            if (this.password === "") {
+                this.$refs.messageComponent.showMessage(
+                    "Password cannot be empty.",
+                    "error"
+                );
+                return;
+            }
+
+            try {
+                const response = await this.$axios.post("/back/api/me/", {
+                    email: this.email,
+                    password: this.password,
+                });
+                this.$refs.messageComponent.showMessage(
+                    "Log in successful!",
+                    "success"
+                );
+                console.log(response);
+            } catch (error) {
+                this.$refs.messageComponent.showMessage(
+                    "Log in failed. Please try again.",
+                    "error"
+                );
+                console.log(error.response.data);
+            }
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: "Roboto", sans-serif;
+    letter-spacing: 0.1em;
+    font-weight: 600;
+    font-size: 1rem;
+    color: darkblue;
+    text-shadow: grey 0px 0px 1px;
+}
+
+form {
+    width: 300px;
+    padding: 4rem;
+    background: linear-gradient(45deg, #9ee1bd, #d1c6ee);
+    border-radius: 2rem;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1rem;
+}
+
+label {
+    margin-bottom: 0.5rem;
+}
+
+input[type="text"],
+input[type="password"] {
+    padding: 0.5rem;
+    border-radius: 0.3rem;
+    border: 1px solid #ced4da;
+    border-radius: 0.5rem;
+    outline: none;
+    font-size: 1.3rem;
+
+    &:hover {
+        border-color: #a7c8ff;
+    }
+
+    &:focus {
+        border-color: #2b6cb0;
+        box-shadow: 0 0 0 0.2rem rgba(43, 108, 176, 0.25);
+    }
+}
+
+button[type="submit"] {
+    margin-top: 1rem;
+    padding: 0.8rem;
+    border-radius: 0.3rem;
+    background-color: #482d8f;
+    color: #fff;
+    border: none;
+    font-size: 1.3rem;
+    width: 100%;
+    border-radius: 0.5rem;
+    font-family: "Roboto", sans-serif;
+    letter-spacing: 0.05em;
+    font-weight: 400;
+    text-transform: uppercase;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #0f1769;
+    }
+
+    &:active {
+        background-color: #406698;
+    }
+}
+</style>
